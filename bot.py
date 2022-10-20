7dac36d9feba03d29c4af321e42c7f988fc42dc2
andrew-znak <komissarov.andrew92@gmail.com>
2022-10-20 22:14:32 +0400

    Added heandler for replied messages

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import logging, os
+import logging, os, re
 import image, phrases
 
 from telegram import Update, ForceReply
@@ -21,14 +21,15 @@ def start(update: Update, context: CallbackContext) -> None:
     )
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
 def get_image(update: Update, context: CallbackContext) -> None:
     """Download image to the bytearray"""
     file = context.bot.get_file(update.message.photo[-1].file_id).download_as_bytearray()
+    final_image = (image.change_image(file))
+    context.bot.send_photo(chat_id=update.message.chat_id,photo=final_image.getvalue())
+
+def get_reply_image(update: Update, context: CallbackContext) -> None:
+    """Download replied image to bytearray"""
+    file = context.bot.get_file(update.message.reply_to_message.photo[-1].file_id).download_as_bytearray()
     final_image = (image.change_image(file))
     context.bot.send_photo(chat_id=update.message.chat_id,photo=final_image.getvalue())
 
@@ -38,8 +39,8 @@ def main() -> None:
     dispatcher = updater.dispatcher
     phrases.read_phrases()
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler((Filters.reply & Filters.regex(r'^[Тт]олик$')) | (Filters.photo & Filters.caption_regex(r'^[Тт]олик$')) , get_image))
+    dispatcher.add_handler(MessageHandler(Filters.photo & Filters.caption_regex(r'^[Тт]олик$'), get_image))
+    dispatcher.add_handler(MessageHandler((Filters.reply & Filters.regex(r'^[Тт]олик$')), get_reply_image))
     # Start the Bot
     updater.start_polling()
 
