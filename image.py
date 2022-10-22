7eb22ce41568767af1491117eef3698588f822a1
andrew-znak <komissarov.andrew92@gmail.com>
2022-10-22 16:24:43 +0400

    Changed font size ratio

--- image.py
+++ image.py
@@ -13,12 +13,12 @@ def change_image(img):
     
     image = Image.open(BytesIO(img))
     image_editable = ImageDraw.Draw(image)
-    font = ImageFont.truetype(font_family, 60)
+    font = ImageFont.truetype(font_family, int(image.size[0]*0.08))
 
     avg_char_width = sum(font.getsize(char)[0] for char in ascii_letters) / len(ascii_letters)
-    max_char_count = int(image.size[0] / avg_char_width)
+    max_char_count = int(image.size[0] / avg_char_width * 1.4)
     text = textwrap.fill(text=text, width=max_char_count)
-    image_editable.text(xy=(image.size[0]/2, image.size[1]-20), fill=(0,0,0), text=text, font=font, anchor='md')
-    image_editable.text(xy=(image.size[0]/2+5, image.size[1]-25), fill=(255,255,255), text=text, font=font, anchor='md')
+    image_editable.text(xy=(image.size[0]/2, image.size[1]), fill=(0,0,0), text=text, font=font, anchor='md')
+    image_editable.text(xy=(image.size[0]/2-2, image.size[1]-2), fill=(255,255,255), text=text, font=font, anchor='md')
     image.save(img_byte_array, format='JPEG')
     return img_byte_array
