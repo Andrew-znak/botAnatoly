f06f5036b9c1e6ff2a557f778958815f1c4bf602
andrew-znak <komissarov.andrew92@gmail.com>
2022-10-25 19:25:42 +0400

    Changed image ratio

--- image.py
+++ image.py
@@ -16,7 +16,7 @@ def change_image(img):
     font = ImageFont.truetype(font_family, int(image.size[0]*0.08))
 
     avg_char_width = sum(font.getsize(char)[0] for char in ascii_letters) / len(ascii_letters)
-    max_char_count = int(image.size[0] / avg_char_width * 1.4)
+    max_char_count = int(image.size[0] / avg_char_width * 1.3)
     text = textwrap.fill(text=text, width=max_char_count)
     image_editable.text(xy=(image.size[0]/2, image.size[1]), fill=(0,0,0), text=text, font=font, anchor='md')
     image_editable.text(xy=(image.size[0]/2-2, image.size[1]-2), fill=(255,255,255), text=text, font=font, anchor='md')
